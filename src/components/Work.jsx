4dccdc6b74f7e1bdfc4464043227d121f2754aa7
Mithun Sivapathasundram <mithunsivapathasundram@outlook.com>
2026-02-26 12:09:49 -0500

    Flipped work experience

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -11,63 +11,7 @@ const Work = () => {
           </p>
         </div>
 
-        <div className="w-full mt-8">
-          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
-            <div className="flex flex-col">
-              <p className="mb-2 font-semibold text-lg">
-                Software Developer (Co-op) -{" "}
-                <span className="text-md text-[#38A5FF]">
-                  Ontario Ministry of Treasury Board Secretariat
-                </span>
-                <p className="text-sm text-gray-300">
-                  January 2024 - August 2024
-                </p>
-              </p>
-              <ol className="text-gray-300 mb-4 text-sm">
-                <li className="py-1">
-                  Developed, debugged, enhanced, and documented several features
-                  to continuously update two legacy web applications for the
-                  Ministry of Finance, aiding millions of Ontarians with tax
-                  concerns.
-                </li>
-                <li className="py-1">
-                  Actively participated in a Scrum-based Agile development team,
-                  contributing to daily stand-up meetings, reporting progress,
-                  handling assignments, addressing technical challenges, and
-                  estimating timelines.
-                </li>
-                <li className="py-1">
-                  Conducted comprehensive testing, including smoke, unit, and
-                  regression tests, ensuring the monthly delivery of
-                  high-quality releases
-                </li>
-              </ol>
-              <div className="flex flex-wrap space-x-2">
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  HTML
-                </span>
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  CSS
-                </span>
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  JavaScript
-                </span>
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  SQL
-                </span>
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  Microsoft Power BI
-                </span>
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  Microsoft Power Automate
-                </span>
-                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
-                  Azure DevOps
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
+        
         <div className="w-full mt-8">
           <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
             <div className="flex flex-col">
@@ -120,6 +64,63 @@ const Work = () => {
                 </span>
                 <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
                   Microsoft SQL Server
+                </span>
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  Azure DevOps
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="w-full mt-8">
+          <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
+            <div className="flex flex-col">
+              <p className="mb-2 font-semibold text-lg">
+                Software Developer (Co-op) -{" "}
+                <span className="text-md text-[#38A5FF]">
+                  Ontario Ministry of Treasury Board Secretariat
+                </span>
+                <p className="text-sm text-gray-300">
+                  January 2024 - August 2024
+                </p>
+              </p>
+              <ol className="text-gray-300 mb-4 text-sm">
+                <li className="py-1">
+                  Developed, debugged, enhanced, and documented several features
+                  to continuously update two legacy web applications for the
+                  Ministry of Finance, aiding millions of Ontarians with tax
+                  concerns.
+                </li>
+                <li className="py-1">
+                  Actively participated in a Scrum-based Agile development team,
+                  contributing to daily stand-up meetings, reporting progress,
+                  handling assignments, addressing technical challenges, and
+                  estimating timelines.
+                </li>
+                <li className="py-1">
+                  Conducted comprehensive testing, including smoke, unit, and
+                  regression tests, ensuring the monthly delivery of
+                  high-quality releases
+                </li>
+              </ol>
+              <div className="flex flex-wrap space-x-2">
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  HTML
+                </span>
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  CSS
+                </span>
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  JavaScript
+                </span>
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  SQL
+                </span>
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  Microsoft Power BI
+                </span>
+                <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
+                  Microsoft Power Automate
                 </span>
                 <span className="mt-1 rounded bg-[#2f343d53] px-2 py-1 text-sm font-medium text-[#38A5FF]">
                   Azure DevOps
